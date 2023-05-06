9943dc37f5aa0b9ee029ee0505ccfa42fe392f60
bendikMichal <mbendik97@gmail.com>
2023-05-06 14:50:25 +0200

    fixed linux build script

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 
 clear
 
-# gcc -o test test.c cpygame.c libs/mouse.c libs/display.c libs/window.c libs/image.c libs/surface.c libs/font.c libs/rect.c libs/key.c libs/clock.c libs/draw.c libs/circle.c -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf
-# gcc -o test test.c -L./buildlib/lib -I./buildlib/include -lSDL2main -lcpygame -lSDL2 -lSDL2_image -lSDL2_ttf
-gcc -g -o test test.c -L./buildlib/lib -I./buildlib/include -lSDL2main -lcpygame -lSDL2 -lSDL2_image -lSDL2_ttf
+# gcc -o test test.c cpygame.c libs/mouse.c libs/display.c libs/window.c libs/image.c libs/surface.c libs/font.c libs/rect.c libs/key.c libs/clock.c libs/draw.c libs/circle.c -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -lm
+# gcc -o test test.c -L./buildlib/lib -I./buildlib/include -lSDL2main -lcpygame -lSDL2 -lSDL2_image -lSDL2_ttf -lm
+gcc -g -o test test.c -L./buildlib/lib -I./buildlib/include -lSDL2main -lcpygame -lSDL2 -lSDL2_image -lSDL2_ttf -lm
 
 mv test ./dlls
 cd dlls
